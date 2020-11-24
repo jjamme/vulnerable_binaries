5caf3e59f6835fb98f8633c16c50ea97d547e6aa
Frank Nguyen <fnguyen2000@hotmail.com>
2020-11-24 14:36:25 +1100

    Add binary and source code

---
+++ 0_bof.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+	char vuln = 'B';
+	char arr[50] = {};
+
+	printf("My favourite letter is: %c\n", vuln);
+	printf("Don't change my favourite letter to A, I hate A\n");
+	printf("What is your favourite letter?\n");
+
+	gets(arr);
+
+	if (vuln == 'A'){
+		printf("Wow you changed my favourite letter :\(\n");
+		return 0;
+	}
+	printf("Here's my favourite character again: %c\n", vuln);
+
+	return 0;
+}
