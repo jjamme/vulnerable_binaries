544e3eef2de63e4a8d6cc5e335a3298ceb051cdc
Frank Nguyen <fnguyen2000@hotmail.com>
2020-11-24 14:36:56 +1100

    Add binary and source code

---
+++ 1_bof.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <strings.h>
+
+int main() {
+	int vuln = 0x1234;
+	char arr[36] = {0};
+
+	printf("Did you know that 4660 is 0x%x in hex?\n", vuln);
+	printf("It's my favourite number.\n");
+	printf("Can you make the number go backwards?\n");
+
+	gets(arr);
+
+	if (vuln == 0x4321) {
+		printf("That is a cool number too, you win!\n");
+		return 0;
+	}
+
+	printf("Here's the number again. 0x%x\n", vuln);
+
+	return 0;
+}
