fb399128ea6a4787091c0e2f7bd6173787ac73da
Frank Nguyen <fnguyen2000@hotmail.com>
2020-11-26 14:32:56 +1100

    Add binary and source code

---
+++ 4_got_skillz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void cool () {
+	printf("Dang that is cool!\n");
+	system("/bin/sh");
+}
+
+void notCool (char *response) {
+	printf("Not impressed at ");
+	printf(response);
+}
+
+void main () {
+	char response[64];
+	printf("If you GOT 5k1llz.\nShow me your sk1llz!\n");
+	fgets(response, sizeof(response), stdin);
+	notCool(response);
+	printf("Well, goodbye!\n");
+
+	exit(0);
+}
