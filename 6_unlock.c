1d9c32b07845db41b83039f9112d104568c7532c
Frank Nguyen <fnguyen2000@hotmail.com>
2020-12-06 00:59:46 +1100

    Add binary and source code

---
+++ 6_unlock.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main () {
+	char name[5] = "WHAT";
+	char response[512];
+
+	printf("My first name is %s?\n", name);
+	printf("My last name is %p\n", &name);
+	printf("Am I lying? (Y/N) ");
+
+	fgets(response, sizeof(response), stdin);
+	response[strlen(response) - 1] = '\0';
+
+	printf("Why do you think ");
+	printf(response);
+
+	if (strcmp(name, "JAME") == 0) {
+		printf("Wow how did you find my name?\n");
+		system("/bin/sh");
+	} else {
+		printf("because you're wrong.\n");
+		printf("That's because my name is %s\n", name);
+	}
+}
