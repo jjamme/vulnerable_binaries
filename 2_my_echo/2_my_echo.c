cade64751c2534e31283b94c7450ffa153007bed
jjamme <fnguyen2000@hotmail.com>
2021-05-18 18:49:12 +1000

    Change wording in the challenge

--- 2_my_echo/2_my_echo.c
+++ 2_my_echo/2_my_echo.c
@@ -1,5 +1,6 @@
-// gcc -m32 -fno-stack-protector -z execstack -no-pie 2_my_echo.c  -o 2_my_echo
+// gcc -m32 -fno-stack-protector -z execstack -no-pie 2_my_echo.c -o 2_my_echo
 
+#include <stdlib.h>
 #include <stdio.h>
 
 int main(){
@@ -7,12 +8,15 @@ int main(){
 	char input[64];
 
 	printf("This is my address %p\n", &flag);
-	printf("Input something cool and I'll repeat it.\n");
+	printf("You can come over if you tell me the password.\n");
 	fgets(input, sizeof(input), stdin);
+	printf("Don't know where you heard ");
 	printf(input);
+	printf("It isn't the password though\n");
 
 	if(flag){
 		printf("Wow how'd you get here?\nWelcome to my house!\n");
+		system("/bin/sh");
 	}
 
 	return 0;
